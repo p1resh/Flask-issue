39e280546c3bb8cba975b8c33e69cf1be9f0b5eb
p1resh <113836759+p1resh@users.noreply.github.com>
2023-02-13 14:42:39 +0530

    Add files via upload

---
+++ First.py
@@ -0,0 +1,20 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+
+
+@app.route('/demo',methods=['POST'])
+def math_operation():
+    if(request.method=='POST'):
+        operation=request.json['operation']
+        num1=request.json['num1']
+        num2=request.json['num2']
+        result= num1+num2
+
+        return "The operation is {} and the result is {}", format(operation, result)
+
+
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=5000)
